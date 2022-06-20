115ab23ea16359961683ed9a414ab6018f7f6e7d
OliverSchlueter <oliverhd1303@gmail.com>
2022-06-20 22:36:54 +0200

    Added abstract classes to implement algorithms

---
+++ src/main/java/algorithms/Algorithm.java
@@ -0,0 +1,20 @@
+package algorithms;
+
+public abstract class Algorithm {
+
+    private final String name;
+    private final String timeComplexity;
+
+    public Algorithm(String name, String timeComplexity){
+        this.name = name;
+        this.timeComplexity = timeComplexity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTimeComplexity() {
+        return timeComplexity;
+    }
+}
