451b3990541ddaf56d364979feb006ff0c2aaee7
OliverSchlueter <oliverhd1303@gmail.com>
2022-07-14 20:11:43 +0200

    Added leetcode problems: 1, 9, 383, 1436, 1678

---
+++ src/main/java/problems/leetcode/GoalParserInterpretation.java
@@ -0,0 +1,13 @@
+package problems.leetcode;
+
+public class GoalParserInterpretation {
+
+    /*
+        https://leetcode.com/problems/goal-parser-interpretation/
+     */
+
+    public String interpret(String command) {
+        return command.replace("()", "o").replace("(", "").replace(")", "");
+    }
+
+}
