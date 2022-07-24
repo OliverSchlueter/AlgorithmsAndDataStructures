4884674d342ca19b9fc568882841c51f5fe8cb4d
OliverSchlueter <oliverhd1303@gmail.com>
2022-07-24 22:18:24 +0200

    Add leetcode problem #881

---
+++ src/main/java/problems/leetcode/BoatsToSavePeople.java
@@ -0,0 +1,32 @@
+package problems.leetcode;
+
+import java.util.Arrays;
+
+public class BoatsToSavePeople {
+
+    /*
+        https://leetcode.com/problems/boats-to-save-people/
+     */
+
+    public int numRescueBoats(int[] people, int limit) {
+        people = Arrays.stream(people).sorted().toArray();
+
+        int boats = 0;
+
+        int left = 0;
+        int right = people.length - 1;
+
+        while(left <= right){
+            int remain = limit - people[right];
+            right--;
+            boats++;
+
+            if(left <= right && remain >= people[left]){
+                left++;
+            }
+        }
+
+        return boats;
+    }
+
+}
