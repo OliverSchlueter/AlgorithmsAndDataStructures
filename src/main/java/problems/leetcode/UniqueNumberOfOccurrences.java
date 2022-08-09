5d095fe2eb8a1a7efbfd0d1a1365713ed473ceaa
OliverSchlueter <oliverhd1303@gmail.com>
2022-08-09 16:13:46 +0200

    Added leetcode problem #1207

---
+++ src/main/java/problems/leetcode/UniqueNumberOfOccurrences.java
@@ -0,0 +1,31 @@
+package problems.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class UniqueNumberOfOccurrences {
+
+    /*
+        https://leetcode.com/problems/unique-number-of-occurrences/
+     */
+
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> occurrences = new HashMap<>();
+
+        for (int i : arr) {
+            if(occurrences.containsKey(i)){
+                occurrences.replace(i, occurrences.get(i) + 1);
+            } else {
+                occurrences.put(i, 1);
+            }
+        }
+
+        // a set only contains unique values and will remove duplicated values
+        Set<Integer> uniqueValues = new HashSet<>(occurrences.values());
+
+        return uniqueValues.size() == occurrences.size();
+    }
+
+}
