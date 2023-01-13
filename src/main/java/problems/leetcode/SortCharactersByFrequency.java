93bf2a795ba97dd88b1e167bd8b1abfa3df00662
OliverSchlueter <oliverhd.1303@gmail.com>
2023-01-13 17:45:12 +0100

    Add leetcode problem #451

---
+++ src/main/java/problems/leetcode/SortCharactersByFrequency.java
@@ -0,0 +1,39 @@
+package problems.leetcode;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SortCharactersByFrequency {
+
+    /*
+        https://leetcode.com/problems/sort-characters-by-frequency/
+     */
+
+    public String frequencySort(String s) {
+        Map<Character, Integer> frequencies = new HashMap<>();
+
+        char[] chars = s.toCharArray();
+
+        for (char c : chars) {
+            frequencies.put(c, frequencies.getOrDefault(c, 0) + 1);
+        }
+
+        String result = "";
+
+        List<Map.Entry<Character, Integer>> sorted = frequencies
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.<Character, Integer>comparingByValue().reversed())
+                .toList();
+
+        for (Map.Entry<Character, Integer> e : sorted) {
+            for (int i = 0; i < e.getValue(); i++) {
+                result += e.getKey();
+            }
+        }
+
+        return result;
+    }
+
+}
