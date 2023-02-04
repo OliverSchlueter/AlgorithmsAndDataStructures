2d69cb5976ee6d09397a8cc732171adb6fe58f62
OliverSchlueter <oliverhd.1303@gmail.com>
2023-02-04 16:24:19 +0100

    Add leetcode problem #371

---
+++ src/main/java/problems/leetcode/SumOfTwoIntegers.java
@@ -0,0 +1,23 @@
+package problems.leetcode;
+
+public class SumOfTwoIntegers {
+
+    /*
+        https://leetcode.com/problems/sum-of-two-integers/
+     */
+
+    public int getSum(int a, int b) {
+        while(b != 0){
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+
+        return a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new SumOfTwoIntegers().getSum(20, 22));
+    }
+
+}
